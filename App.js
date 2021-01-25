b8fde94e88c47686ba5ffc64b8dfc24a1eef205d
ArunaRandika <randiikaroxxs98@gmail.com>
2021-01-25 18:26:34 +0530

    test 2 done

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Testing 1111</Text>
+      <Text>Testing 2222</Text>
       <StatusBar style="auto" />
     </View>
   );
